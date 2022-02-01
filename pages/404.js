703fb86aadec4283801ef9e1139f0bb54411cdbf
Willian Alberto Lauber <e018194@celesc.com.br>
2022-02-01 16:18:37 -0300

    feat: nextjs Link class navigation

---
+++ pages/404.js
@@ -0,0 +1,4 @@
+import Link from "next/link";
+export default function ChatPage() {
+  return <div>Oooops... 404</div>;
+}
---
+++ pages/chat.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function ChatPage() {
+  return (
+    <div>
+      Chat Page
+      <Link href="/">
+        <a>Sair </a>
+      </Link>
+    </div>
+  );
+}
